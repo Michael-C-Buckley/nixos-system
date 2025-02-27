4282d33d46d80ef1b71594ea3abd4270ed984894
Michael Buckley <michaelcbuckley@proton.me>
2025-02-26 21:37:40 -0500

    Added some basic OSPF to X570 for my home networking needs

--- hosts/x570/frr.nix
+++ hosts/x570/frr.nix
@@ -2,8 +2,18 @@ _ : {
   # Default Nixos will have standard priority, force to override
   # environment.etc."frr/frr.conf".source = lib.mkForce config.age.secrets.frr.path;
 
+  environment.etc."frr/frr.conf".text = lib.mkForce ''
+  ip forwarding
+  router ospf
+   ospf router-id 192.168.48.10
+  int wlp7s0
+   ip ospf area 0
+  int lo
+   ip ospf area 0
+   ip ospf passive
+  '';
+
   services.frr = {
-    # WIP: fd limits don't work still
     bgpd.enable = true;
     eigrpd.enable = true;
     ospfd.enable = true;
