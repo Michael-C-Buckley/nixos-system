39924803d19fdecdb49e9664721667011795dd69
Michael Buckley <michaelcbuckley@proton.me>
2025-03-19 07:22:21 -0400

    Streamlined T14's filesystems

--- hosts/t14/hardware/filesystems.nix
+++ hosts/t14/hardware/filesystems.nix
@@ -1,4 +1,14 @@
-{...}: {
+{...}: let
+  zfsFs = name: {
+    device = "rpool/root/${name}";
+    fsType = "zfs";
+  };
+  bindMnt = device : {
+    device = device;
+    fsType = "none";
+    options = ["bind"];
+  };
+in {
   fileSystems = {
     "/boot" = {
       device = "/dev/disk/by-uuid/B227-3C56";
@@ -6,20 +16,9 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-    "/" = {
-      device = "rpool/root/nixos-2411";
-      fsType = "zfs";
-    };
-
-    "/home" = {
-      device = "rpool/root/home";
-      fsType = "zfs";
-    };
-
-    "/nix" = {
-      device = "rpool/root/nix";
-      fsType = "zfs";
-    };
+    "/" = zfsFs "nixos-2411";
+    "/home"= zfsFs "home";
+    "/nix" = zfsFs "nix";
 
     "/drives/xfs-pool" = {
       device = "/dev/disk/by-uuid/0e9f0e00-eca4-4d8f-a12f-7534b8939d9d";
@@ -31,26 +30,9 @@
     };
 
     # Bind mounts
-    "/var/lib/libvirt" = {
-      device = "/drives/xfs-pool/libvirt";
-      fsType = "none";
-      options = ["bind"];
-    };
-    "/var/lib/docker" = {
-      device = "/drives/xfs-pool/docker";
-      fsType = "none";
-      options = ["bind"];
-    };
-
-    "/home/michael/Downloads" = {
-      device = "/drives/ext4-pool/downloads";
-      fsType = "none";
-      options = ["bind"];
-    };
-    "/home/michael/Downloads/ISOs" = {
-      device = "/drives/xfs-pool/downloads";
-      fsType = "none";
-      options = ["bind"];
-    };
+    "/var/lib/libvirt" = bindMnt "/drives/xfs-pool/libvirt";
+    "/var/lib/docker" = bindMnt "/drives/xfs-pool/docker";
+    "/home/michael/Downloads" = bindMnt "/drives/ext4-pool/downloads";
+    "/home/michael/Downloads/ISOs" = bindMnt "/drives/xfs-pool/downloads";
   };
 }
