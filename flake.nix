549a0efc6d09170a06d5ee9ceeb62ddad60389f4
Michael Buckley <michaelcbuckley@proton.me>
2025-03-19 07:15:36 -0400

    Reorganized inputs and updated them

--- flake.nix
+++ flake.nix
@@ -7,37 +7,24 @@
     nixpkgs.follows = "cosmic/nixpkgs";
     nixpkgs-stable.follows = "cosmic/nixpkgs-stable";
 
-    hyprland.url = "github:hyprwm/hyprland";
-
     # User configs
     michael-home = {
       url = "github:Michael-C-Buckley/home-config";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "";
     };
+    hyprland.url = "github:hyprwm/hyprland";
     hjem.follows = "michael-home/hjem";
+    wfetch.url = "github:iynaix/wfetch";
+
+    # Custom Modules
+    nix-devshells.url = "github:Michael-C-Buckley/nix-devshells";
+    nix-secrets.url = "git+ssh://git@github.com/Michael-C-Buckley/nix-secrets";
+    nixos-modules.url = "github:Michael-C-Buckley/nixos-modules";
 
     # Utilities
     ucodenix.url = "github:e-tho/ucodenix";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
-    wfetch.url = "github:iynaix/wfetch";
-    
-    nix-devshells = {
-      url = "github:Michael-C-Buckley/nix-devshells";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-secrets = {
-      url = "git+ssh://git@github.com/Michael-C-Buckley/nix-secrets";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nix-devshells.follows = "nix-devshells";
-    };
-
-    nixos-modules = {
-      url = "github:Michael-C-Buckley/nixos-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nix-devshells.follows = "nix-devshells";
-    };
 
     ragenix = {
       url = "github:yaxitech/ragenix";
