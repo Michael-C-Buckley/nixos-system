c939f986204245262a790dbb341fa9741efb5334
Michael Buckley <michaelcbuckley@proton.me>
2025-03-24 07:30:01 -0400

    Updated users file

--- system/users.nix
+++ system/users.nix
@@ -1,25 +1,19 @@
 {pkgs, inputs, ...}: let
-  sshKeys = inputs.nix-secrets.nixosModules.sshKeys;
-  allGroups = [ "networkmanager" "wheel" "video" "wireshark" ];
+  allGroups = ["networkmanager" "wheel" "video" "wireshark"];
 in {
-  users = {
-    users = {
-      root = {
-        openssh.authorizedKeys.keys = sshKeys.michael.personal;
-      };
-      michael = {
-        isNormalUser = true;
-        extraGroups = allGroups;
-        initialHashedPassword = "$y$j9T$B7X2OiGnlXlAHCX77SKVL/$uhHb/UP4pHqEeZIzubxruA.U2ZYG3AK1x0xn/QcbLhC";
-        openssh.authorizedKeys.keys = sshKeys.michael.personal;
-      };
-      shawn = {
-        shell = pkgs.zsh;
-        isNormalUser = true;
-        extraGroups = allGroups;
-        initialHashedPassword = "$y$j9T$B7X2OiGnlXlAHCX77SKVL/$uhHb/UP4pHqEeZIzubxruA.U2ZYG3AK1x0xn/QcbLhC";
-        openssh.authorizedKeys.keys = sshKeys.shawn.personal;
-      };
+  imports = [
+    inputs.nix-secrets.nixosModules.users
+  ];
+
+  users.users = {
+    michael = {
+      isNormalUser = true;
+      extraGroups = allGroups;
+    };
+    shawn = {
+      shell = pkgs.zsh;
+      isNormalUser = true;
+      extraGroups = allGroups;
     };
   };
 }
